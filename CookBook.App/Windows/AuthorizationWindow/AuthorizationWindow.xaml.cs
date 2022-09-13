29f49c9c4fd3fc977d9b2e648ba710785f5c47ee
Starinin Andrey <starininandrey@gmail.com>
2022-09-13 18:06:22 +0300

    Done AuthorizationWindow

--- CookBook.App/Windows/AuthorizationWindow/AuthorizationWindow.xaml.cs
+++ CookBook.App/Windows/AuthorizationWindow/AuthorizationWindow.xaml.cs
@@ -8,4 +8,27 @@ public partial class AuthorizationWindow : Window
     {
         InitializeComponent();
     }
+
+    private void ButtonClear_OnClick(object sender, RoutedEventArgs e)
+    {
+        InputLogin.Clear();
+        InputPassword.Clear();
+    }
+
+    private void ButtonLogin_OnClick(object sender, RoutedEventArgs e)
+    {
+        var login = InputLogin.Text;
+        var password = InputPassword.Password;
+
+        if (login == "login" && password == "123")
+        {
+            MessageBox.Show("Вы успешно авторизовались!");
+            new MainWindow().Show();
+            this.Close();
+        }
+        else
+        {
+            MessageBox.Show("Вы ввели неправильно логин или пароль");
+        }
+    }
 }
