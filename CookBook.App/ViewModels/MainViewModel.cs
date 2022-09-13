12520845f446d0b668bfa78b32239cd1d136f01f
Starinin Andrey <starininandrey@gmail.com>
2022-09-13 18:50:56 +0300

    Add MainViewModel

---
+++ CookBook.App/ViewModels/MainViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.ObjectModel;
+using CookBook.App.Models;
+
+namespace CookBook.App.ViewModels;
+
+public class MainViewModel : BaseViewModel
+{
+    public ObservableCollection<Recipe> Recipes { get; set; }
+
+    private Recipe _recipe;
+    public Recipe Recipe
+    {
+        get => _recipe;
+        set => SetField(ref _recipe, value);
+    }
+}
