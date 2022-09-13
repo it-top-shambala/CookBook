b9a9028189edd185ec62264672ccc50d332f8708
Starinin Andrey <starininandrey@gmail.com>
2022-09-13 17:13:51 +0300

    Add LambdaCommand

---
+++ CookBook.App/Commands/LambdaCommand.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace CookBook.App.Commands;
+
+public class LambdaCommand : ICommand
+{
+    private readonly Predicate<object> _canExecute;
+    private readonly Action<object> _execute;
+
+    public LambdaCommand(Action<object> execute, Predicate<object> canExecute)
+    {
+        _canExecute = canExecute;
+        _execute = execute;
+    }
+
+    public bool CanExecute(object parameter)
+    {
+        return _canExecute.Invoke(parameter);
+    }
+
+    public void Execute(object parameter)
+    {
+        _execute.Invoke(parameter);
+    }
+
+    public event EventHandler CanExecuteChanged;
+}
