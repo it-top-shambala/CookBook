1178794f984617244c7245776162260866bbd3eb
Starinin Andrey <starininandrey@gmail.com>
2022-09-13 18:31:51 +0300

    Add models

---
+++ CookBook.App/Models/Ingredient.cs
@@ -0,0 +1,8 @@
+﻿namespace CookBook.App.Models;
+
+public class Ingredient
+{
+    public string Name { get; set; }
+    public string Measure { get; set; }
+    public double Amount { get; set; }
+}
---
+++ CookBook.App/Models/Recipe.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CookBook.App.Models;
+
+public class Recipe
+{
+    public string Name { get; set; }
+    public List<Ingredient> Ingredients { get; set; }
+    public string Description { get; set; }
+    public Uri Image { get; set; }
+}
