86f3a8257980356beb85abb0338a556ed45ec0d4
Starinin Andrey <starininandrey@gmail.com>
2022-09-20 18:49:04 +0300

    Add InputComponent

---
+++ CookBook.App/Components/InputComponent/InputComponent.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace CookBook.App.Components.InputComponent;
+
+public partial class InputComponent : UserControl
+{
+    public string LabelName
+    {
+        get => GetValue(LabelNameProperty).ToString();
+        set => SetValue(LabelNameProperty, value);
+    }
+
+    public string LabelValue
+    {
+        get => GetValue(LabelValueProperty).ToString();
+        set => SetValue(LabelValueProperty, value);
+    }
+
+    public static readonly DependencyProperty LabelNameProperty = DependencyProperty.Register(nameof(LabelName), typeof(string), typeof(InputComponent), new FrameworkPropertyMetadata(string.Empty, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+    public static readonly DependencyProperty LabelValueProperty = DependencyProperty.Register(nameof(LabelValue), typeof(string), typeof(InputComponent), new FrameworkPropertyMetadata(string.Empty, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+    public InputComponent()
+    {
+        InitializeComponent();
+    }
+}
+
