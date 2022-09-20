4861229512baf660a619a0130782a473875c8a24
Starinin Andrey <starininandrey@gmail.com>
2022-09-20 18:49:51 +0300

    Add ExportToJson

--- CookBook.App/Models/Recipes.cs
+++ CookBook.App/Models/Recipes.cs
@@ -1,14 +1,28 @@
 ﻿using System.Collections.Generic;
 using System.IO;
+using System.Text.Encodings.Web;
 using System.Text.Json;
+using System.Text.Unicode;
 
 namespace CookBook.App.Models;
 
 public static class Recipes
 {
+    private const string Path = "./Assets/recipes.json";
+
     public static IEnumerable<Recipe> ImportFromJson()
     {
-        var json = File.ReadAllText("./Assets/recipes.json");
+        var json = File.ReadAllText(Path);
         return JsonSerializer.Deserialize<IEnumerable<Recipe>>(json);
+    }
+
+    public static void ExportToJson(IEnumerable<Recipe> recipes)
+    {
+        var json = JsonSerializer.Serialize(recipes, new JsonSerializerOptions
+        {
+            Encoder = JavaScriptEncoder.Create(UnicodeRanges.BasicLatin, UnicodeRanges.Cyrillic),
+            WriteIndented = true
+        });
+        File.WriteAllText(Path, json);
     }
 }
