e33511d42b941a7a71800cd4706d206bc5498aa4
Starinin Andrey <starininandrey@gmail.com>
2022-09-20 17:10:27 +0300

    Add OutputComponent

---
+++ CookBook.App/Components/OutputComponent/OutputComponent.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace CookBook.App.Components.OutputComponent;
+
+public partial class OutputComponent : UserControl
+{
+    public string LabelName
+    {
+        get => GetValue(LabelNameProperty).ToString();
+        set => SetValue(LabelNameProperty, value);
+    }
+
+    public string LabelValue
+    {
+        get => GetValue(LabelValueProperty).ToString();
+        set => SetValue(LabelValueProperty, value);
+    }
+
+
+    public readonly DependencyProperty LabelNameProperty;
+    public readonly DependencyProperty LabelValueProperty;
+    public OutputComponent()
+    {
+        LabelNameProperty = DependencyProperty.Register(nameof(LabelName), typeof(string), typeof(OutputComponent));
+        LabelValueProperty = DependencyProperty.Register(nameof(LabelValue), typeof(string), typeof(OutputComponent));
+        InitializeComponent();
+    }
+}
+
