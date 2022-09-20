b03a969304b95177fe0c1e1e0fc042bc69a89eb8
Starinin Andrey <starininandrey@gmail.com>
2022-09-20 17:09:46 +0300

    Add EditWindow

---
+++ CookBook.App/Windows/EditWindow/EditWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace CookBook.App.Windows.EditWindow;
+
+public partial class EditWindow : Window
+{
+    public EditWindow()
+    {
+        InitializeComponent();
+    }
+}
+
